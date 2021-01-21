4bb443cacea5329b6d2341c7797ba5413510fd9b
Alex Lanchais <alex@kettlenyc.com>
2021-01-21 17:24:23 -0500

    Mobile / Reach a counselor CTA scroll behavior, part of #10296

--- src/theme/js/frontend/nav.js
+++ src/theme/js/frontend/nav.js
@@ -1,12 +1,25 @@
 import $ from 'jquery';
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 
 const $body = $('body');
 
 const isBodyOnTop = throttle(
-	() => $body[$(document).scrollTop() > 0 ? 'removeClass' : 'addClass']('on-top'),
+	() => {
+		$body[$(document).scrollTop() > 0 ? 'removeClass' : 'addClass']('on-top')
+		$body.removeClass('is-not-scrolling')
+	},
 	100
+);
+const isScrolling = debounce(
+	() => {
+		$body.addClass('is-not-scrolling')
+	},
+	2000
 );
 
 $(isBodyOnTop); // On load
+$(isScrolling); // On load
+// $body.addClass('is-not-scrolling');
 $(window).on('scroll', isBodyOnTop); // On scroll
+$(window).on('scroll', isScrolling); // On scroll
