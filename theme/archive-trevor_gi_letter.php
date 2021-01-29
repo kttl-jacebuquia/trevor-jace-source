6813687be45ed96f3ef94910aac78931f2ca483f
Alex Lanchais <alex@kettlenyc.com>
2021-01-29 18:00:38 -0500

    Desktop / Advocate For Change / Remove outline for bills cards on initial LP, resolves #10336

--- theme/archive-trevor_gi_letter.php
+++ theme/archive-trevor_gi_letter.php
@@ -10,7 +10,9 @@
 
 <main id="site-content" role="main" class="bg-white">
 	<div class="container mx-auto">
-		<?= \TrevorWP\Theme\Helper\Tile_Grid::posts( $wp_query->posts ) ?>
+		<?= \TrevorWP\Theme\Helper\Tile_Grid::posts( $wp_query->posts, [
+			'tileClass' => ['border', 'border-blue_green', 'border-opacity-50']
+		] ) ?>
 	</div>
 </main> <!-- #site-content -->
 
