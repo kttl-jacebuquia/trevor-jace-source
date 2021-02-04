791ecf7b88e23c4a103096a508aa2a08b321d33b
Evrim Cabuk <evrim.cabuk@wearekettle.com>
2021-02-04 14:51:48 -0500

    Updates rc grid

--- theme/rc/search.php
+++ theme/rc/search.php
@@ -12,60 +12,62 @@ $no_results = empty( $wp_query->found_posts );
 			</form>
 		</div>
 
-		<div class="search-results-container flex flex-col flex-1 trevor-grid-default-container">
-			<h1 class="search-results-list-title text-white text-center text-base mb-5 md:mb-10 lg:mb-10">
-				<?php if ( $no_results ) { ?>
-					<span class="leading-px20 font-medium md:font-light md:text-px18 md:leading-px26 lg:font-normal lg:text-px22 lg:leading-px32 lg:tracking-px05">
+		<div class="search-results-container flex flex-col flex-1">
+			<div class="trevor-grid-default-container">
+				<h1 class="search-results-list-title text-white text-center text-base mb-5 md:mb-10 lg:mb-10">
+					<?php if ( $no_results ) { ?>
+						<span class="leading-px20 font-medium md:font-light md:text-px18 md:leading-px26 lg:font-normal lg:text-px22 lg:leading-px32 lg:tracking-px05">
 						There are no results for “<?= get_search_query() ?>”
 					</span>
-				<?php } else { ?>
-					<span class="leading-px22 font-normal md:text-px18 md:leading-px24 lg:text-px22 lg:leading-px32 lg:tracking-px05">
+					<?php } else { ?>
+						<span class="leading-px22 font-normal md:text-px18 md:leading-px24 lg:text-px22 lg:leading-px32 lg:tracking-px05">
 						Search Results for “<span class="font-extrabold md:font-normal"><?= get_search_query() ?></span>”
 					</span>
-				<?php } ?>
-			</h1>
-			<?php if ( have_posts() ) { ?>
-				<div class="search-results-list trevor-grid-default">
-					<?php while ( have_posts() ) {
-						the_post();
-						echo \TrevorWP\Theme\Helper\Card::post( get_post() );
-						?>
 					<?php } ?>
-
-					<div class="trevor-pagination-default">
-						<?php get_template_part( 'template-parts/pagination' ); ?>
-					</div>
-				</div>
-			<?php } else if ( $no_results ) { ?>
-				<div class="search-results-popular-wrap flex-1 text-white">
-					<h2 class="font-semibold text-px26 leading-px32 -tracking-px05 mb-2 md:text-px32 md:leading-px42 md:tracking-em_001 lg:text-px46 lg:leading-px56">
-						Popular Searches
-					</h2>
-					<p class="text-px18 leading-px24 -tracking-em005 mb-7 md:tracking-em001 lg:text-px26 lg:leading-px36 lg:mb-10">
-						Here’s some popular search terms other people have been exploring:
-					</p>
-					<div class="flex flex-wrap mb-20 lg:mb-40">
-						<?php foreach (
-								[
-										'Arbitrary Data',
-										'Not Implemented!',
-										'Coming Out',
-										'Mental Health',
-										'Gay',
-										'Lesbian',
-										'Bisexual',
-										'Queer',
-										'Nonbinary',
-										'Gender',
-										'Transgender',
-								] as $search
-						) { ?>
-							<a href="<?= esc_url( \TrevorWP\CPT\RC\RC_Object::get_search_url( $search ) ) ?>"
-							   class="bg-violet-light text-indigo font-medium text-px14 leading-px18 tracking-em001 rounded-full py-1.5 px-3.5 mb-3 mr-2"><?= esc_html( $search ) ?></a>
+				</h1>
+				<?php if ( have_posts() ) { ?>
+					<div class="search-results-list trevor-grid-default">
+						<?php while ( have_posts() ) {
+							the_post();
+							echo \TrevorWP\Theme\Helper\Card::post( get_post() );
+							?>
 						<?php } ?>
+
+						<div class="trevor-pagination-default">
+							<?php get_template_part( 'template-parts/pagination' ); ?>
+						</div>
 					</div>
-				</div>
-			<?php } ?>
+				<?php } else if ( $no_results ) { ?>
+					<div class="search-results-popular-wrap flex-1 text-white">
+						<h2 class="font-semibold text-px26 leading-px32 -tracking-px05 mb-2 md:text-px32 md:leading-px42 md:tracking-em_001 lg:text-px46 lg:leading-px56">
+							Popular Searches
+						</h2>
+						<p class="text-px18 leading-px24 -tracking-em005 mb-7 md:tracking-em001 lg:text-px26 lg:leading-px36 lg:mb-10">
+							Here’s some popular search terms other people have been exploring:
+						</p>
+						<div class="flex flex-wrap mb-20 lg:mb-40">
+							<?php foreach (
+									[
+											'Arbitrary Data',
+											'Not Implemented!',
+											'Coming Out',
+											'Mental Health',
+											'Gay',
+											'Lesbian',
+											'Bisexual',
+											'Queer',
+											'Nonbinary',
+											'Gender',
+											'Transgender',
+									] as $search
+							) { ?>
+								<a href="<?= esc_url( \TrevorWP\CPT\RC\RC_Object::get_search_url( $search ) ) ?>"
+								   class="bg-violet-light text-indigo font-medium text-px14 leading-px18 tracking-em001 rounded-full py-1.5 px-3.5 mb-3 mr-2"><?= esc_html( $search ) ?></a>
+							<?php } ?>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
 	</div>
 	<?= \TrevorWP\Theme\Helper\Categories::render_rc_featured_hero(); ?>
--- theme/taxonomy.php
+++ theme/taxonomy.php
@@ -1,27 +1,29 @@
 <?php get_header(); ?>
 
 <main id="site-content" role="main">
-	<div class="container trevor-grid-default-container">
-		<div class="flex flex-col text-center items-center text-white mb-8 md:items-start md:text-left">
-			<?php
-			/** @var \WP_Term $term */
-			$term = get_queried_object();
-			if ( $term ) { ?>
-				<h1 class="font-bold text-px32 leading-px42 my-6 md:tracking-em001 lg:text-px46 lg:leading-px56"><?= esc_html( $term->name ) ?></h1>
-				<?php if ( ! empty( $term->description ) ) { ?>
-					<p class="font-normal text-px20 leading-px26 text-center mb-8 md:text-left md:text-px22 md:leading-px32 md:tracking-em005 lg:text-px26 lg:leading-px36"><?= esc_html( $term->description ) ?></p>
+	<div class="container mx-auto">
+		<div class="trevor-grid-default-container">
+			<div class="flex flex-col text-center items-center text-white mb-8 md:items-start md:text-left">
+				<?php
+				/** @var \WP_Term $term */
+				$term = get_queried_object();
+				if ( $term ) { ?>
+					<h1 class="font-bold text-px32 leading-px42 my-6 md:tracking-em001 lg:text-px46 lg:leading-px56"><?= esc_html( $term->name ) ?></h1>
+					<?php if ( ! empty( $term->description ) ) { ?>
+						<p class="font-normal text-px20 leading-px26 text-center mb-8 md:text-left md:text-px22 md:leading-px32 md:tracking-em005 lg:text-px26 lg:leading-px36"><?= esc_html( $term->description ) ?></p>
+					<?php } ?>
 				<?php } ?>
-			<?php } ?>
-		</div>
-		<div class="trevor-grid-default">
-			<?php while ( have_posts() ) {
-				the_post();
-				echo \TrevorWP\Theme\Helper\Card::post( get_post() );
-				?>
-			<?php } ?>
+			</div>
+			<div class="trevor-grid-default">
+				<?php while ( have_posts() ) {
+					the_post();
+					echo \TrevorWP\Theme\Helper\Card::post( get_post() );
+					?>
+				<?php } ?>
 
-			<div class="trevor-pagination-default">
-				<?php get_template_part( 'template-parts/pagination' ); ?>
+				<div class="trevor-pagination-default">
+					<?php get_template_part( 'template-parts/pagination' ); ?>
+				</div>
 			</div>
 		</div>
 	</div>
