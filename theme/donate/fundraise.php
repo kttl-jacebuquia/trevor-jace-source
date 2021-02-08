67a7587ff98abe885cb47948f5938be55cc947b5
McRey S. Marcelo <mcrey.marcelo@codeandtheory.com>
2021-02-08 21:32:53 +0800

    Fundraise - Add Recirculation Block (#38)

--- theme/donate/fundraise.php
+++ theme/donate/fundraise.php
@@ -7,7 +7,19 @@ use \TrevorWP\Theme\Customizer\Fundraise;
 
 ?>
 	<main id="site-content" role="main" class="site-content">
-		<h1>Fundraise</h1>
+
+		<?php /* Recirculation */ ?>
+		<?php $circulation_title = Fundraise::get_val( Fundraise::SETTING_OTHER_TITLE ); ?>
+		<?php /* Two Up: Other Ways to Help   */ ?>
+		<div class="other-ways">
+			<div class="container mx-auto text-center">
+				<h3 class="mb-px60 md:mb-px40 lg:mb-px90"><?= $circulation_title ?></h3>
+				<div class="grid grid-cols-1 gap-y-6 max-w-lg mx-auto lg:grid-cols-2 lg:gap-x-7 lg:max-w-none xl:max-w-px1240">
+					<?= Helper\Circulation_Card::render_fundraiser(); ?>
+					<?= Helper\Circulation_Card::render_counselor(); ?>
+				</div>
+			</div>
+		</div>
 	</main>
 
 <?php get_footer();
