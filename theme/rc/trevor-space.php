1bc79c84f22546791a36e59e0e5872a1480d8277
Mark Lorence Munsayac <munsayacmlj@gmail.com>
2021-02-15 15:39:36 +0800

    TrevorSpace text sizes and padding adjustments.

--- theme/rc/trevor-space.php
+++ theme/rc/trevor-space.php
@@ -23,12 +23,12 @@ $online_count_txt = $user_count > $user_threshold
 				<i class="trevor-ti-online mr-2"></i> <?= $online_count_txt ?>
 			</h5>
 			<h1 class="heading-lg-tilted text-indigo mb-4"><?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_TITLE ) ?></h1>
-			<p class="text-px18 mb-7 text-indigo md:text-px20 md:leading-px30 md:mx-px88 md:mb-5 lg:text-px26 lg:leading-px36 lg:mb-14 lg:mx-64"><?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_DESC ) ?></p>
+			<p class="font-normal text-px18 mb-7 text-indigo md:text-px20 md:leading-px30 md:mx-px88 md:mb-5 lg:text-px22 lg:leading-px36 lg:mb-14 lg:mx-64"><?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_DESC ) ?></p>
 
 			<a href="<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_JOIN_URL ) ?>"
 			   rel="noreferrer noopener"
 			   target="_blank"
-			   class="block text-white font-bold text-px18 leading-px24 bg-indigo py-4 px-8 rounded-px10 mx-auto md:text-px16 md:leading-px22 lg:mb-9 lg:text-px22 lg:leading-px22 lg:px-20 lg:py-6">
+			   class="block text-white font-bold text-px18 leading-px24 bg-indigo py-4 px-8 rounded-px10 mx-auto md:text-px16 md:leading-px22 lg:mb-9 lg:text-px18 lg:leading-px22 lg:px-20 lg:py-6">
 				<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_JOIN_CTA ) ?>
 			</a>
 			<div class="login-row mt-5 mb-20 text-indigo md:mb-28 lg:mb-40">
@@ -53,10 +53,10 @@ $online_count_txt = $user_count > $user_threshold
 					<div class="text-indigo font-bold text-px14 leading-px18 tracking-px05 mb-2 md:tracking-em001 uppercase lg:mb-4">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_1_TITLE_TOP ) ?>
 					</div>
-					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px40 lg:leading-px50">
+					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px32 lg:leading-px50">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_1_TITLE ) ?>
 					</h3>
-					<p class="text-px20 leading-px30 md:text-px18 lg:text-10 lg:text-px22 lg:leading-px32">
+					<p class="text-px20 leading-px30 md:text-px18 lg:text-10 lg:text-px22 lg:leading-px32 font-normal">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_1_DESC ) ?>
 					</p>
 				</div>
@@ -75,10 +75,10 @@ $online_count_txt = $user_count > $user_threshold
 					<div class="text-indigo font-bold text-px14 leading-px18 tracking-px05 mb-2 md:tracking-em001 uppercase lg:mb-4">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_2_TITLE_TOP ) ?>
 					</div>
-					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px40 lg:leading-px50">
+					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px32 lg:leading-px50">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_2_TITLE ) ?>
 					</h3>
-					<p class="text-px20 leading-px30 mb-6 md:leading-px32 lg:text-px22 lg:leading-px32">
+					<p class="text-px20 leading-px30 mb-6 md:leading-px32 lg:text-px22 lg:leading-px32 font-normal">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_2_DESC ) ?>
 					</p>
 				</div>
@@ -97,10 +97,10 @@ $online_count_txt = $user_count > $user_threshold
 					<div class="text-indigo font-bold text-px14 leading-px18 tracking-px05 mb-2 md:tracking-em001 uppercase lg:mb-4">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_3_TITLE_TOP ) ?>
 					</div>
-					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px40 lg:leading-px50">
+					<h3 class="font-bold text-px34 leading-px40 mb-5 md:text-px30 md:leading-px40 md:mb-4 lg:text-px32 lg:leading-px50">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_3_TITLE ) ?>
 					</h3>
-					<p class="text-px20 leading-px30 mb-6 md:text-px18 lg:text-px22 lg:leading-px32">
+					<p class="text-px20 leading-px30 mb-6 md:text-px18 lg:text-px22 lg:leading-px32 font-normal">
 						<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_3_DESC ) ?>
 					</p>
 				</div>
@@ -110,10 +110,10 @@ $online_count_txt = $user_count > $user_threshold
 
 	<div class="bg-white">
 		<div class="container mx-auto text-center text-indigo site-content-inner pt-20 pb-16 lg:pt-36 lg:pb-28">
-			<h3 class="text-px40 leading-px52 tracking-px_015 font-semibold mb-4 md:text-px32 md:leading-px42 lg:text-px46 lg:leading-px56">
+			<h3 class="text-px40 leading-px52 tracking-px_015 font-semibold mb-4 md:text-px32 md:leading-px42 lg:text-px32 lg:leading-px56">
 				<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_CIRCULATION_TITLE ) ?>
 			</h3>
-			<p class="text-px20 leading-px30 tracking-em005 mb-14 md:text-px16 md:leading-px22 md:mb-px50 md:mx-24 lg:text-px26 lg:leading-px36 lg:mb-20 lg:mx-64">
+			<p class="font-normal text-px20 leading-px30 tracking-em005 mb-14 md:text-px16 md:leading-px22 md:mb-px50 md:mx-24 lg:text-px22 lg:leading-px36 lg:mb-20 lg:mx-64">
 				<?= Resource_Center::get_val( Resource_Center::SETTING_TREVORSPACE_CIRCULATION_DESC ) ?>
 			</p>
 
