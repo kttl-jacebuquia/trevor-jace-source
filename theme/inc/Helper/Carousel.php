256ea4e707e74213da92cdb8749a0e358f794627
Mark Lorence Munsayac <munsayacmlj@gmail.com>
2021-02-23 16:13:03 +0800

    fix the testimonial carousel styling (#159)

--- theme/inc/Helper/Carousel.php
+++ theme/inc/Helper/Carousel.php
@@ -245,13 +245,13 @@ class Carousel {
 						<div class="carousel-right-arrow-pane h-full w-1/6 px-4 relative" data-direction='right' aria-label="Next Slide" role="button"></div>
 					</div>
 					<div class="swiper-container h-full">
-						<div class="flex flex-row justify-center w-full md:mt-px40 lg:mt-0">
+						<div class="flex flex-row justify-center w-full mt-px40 lg:mt-0">
 							<i class="trevor-ti-quote-open -mt-2 mr-0.5 lg:text-px28 lg:mr-2"></i>
 							<i class="trevor-ti-quote-close lg:text-px28"></i>
 						</div>
 						<div class="swiper-wrapper">
 							<?php foreach ( $data as $entry ): ?>
-								<div class="swiper-slide px-4 pt-5 pb-12 lg:px-8 lg:pt-8">
+								<div class="swiper-slide px-4 pt-5 pb-14 lg:px-8 lg:pt-8">
 									<figure class="text-center text-teal-dark flex flex-col justify-between h-full md:w-full md:mx-auto">
 										<blockquote
 												class="font-bold text-center text-3xl mb-4 md:text-px20 md:leading-px26 lg:text-px30 lg:leading-px40">
