4f5cb6239ff95326fc2cb2002f422dd8854613f6
McRey S. Marcelo <mcrey.marcelo@codeandtheory.com>
2021-02-24 15:10:17 +0800

    #132: Adjust FAQ Styling (#177)

--- src/theme/js/frontend/features/faqs/index.js
+++ src/theme/js/frontend/features/faqs/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 export default function faqToggle($btn) {
-	const slideDuration = 600;
+	const slideDuration = 50;
 	$btn.toggleClass('is-open');
 	$btn.next().slideToggle(slideDuration);
 }
