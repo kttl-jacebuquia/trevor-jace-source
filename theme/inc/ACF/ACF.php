0ec1c84319aa9e95442e05f8e16d6b16beef716f
Mark Lorence Munsayac <munsayacmlj@gmail.com>
2021-04-20 19:57:34 +0800

    Create the address Gutenberg block (#460)
    * Create the address gutenberg block, Fix the flex direction for mobile breakpoint

--- theme/inc/ACF/ACF.php
+++ theme/inc/ACF/ACF.php
@@ -22,6 +22,7 @@ class ACF {
 		Field_Group\Post_Grid::class,
 		Field_Group\Info_Card::class,
 		Field_Group\Info_Card_Grid::class,
+		Field_Group\Address::class,
 		// Page Specific
 		Field_Group\Page_Header::class,
 		Field_Group\Team_Member::class,
---
+++ theme/inc/ACF/Field_Group/Address.php
@@ -0,0 +1,104 @@
+<?php namespace TrevorWP\Theme\ACF\Field_Group;
+
+use TrevorWP\Theme\Helper;
+
+class Address extends A_Field_Group implements I_Block {
+	const FIELD_TITLE = 'title';
+	const FIELD_ENTRIES = 'entries';
+	const FIELD_ENTRY_TITLE = 'entry_title';
+	const FIELD_ENTRY_LINES = 'entry_lines';
+	const FIELD_LINE = 'line';
+	const FIELD_ATTR = 'attr';
+
+
+	protected static function prepare_register_args(): array {
+		$title              = static::gen_field_key( static::FIELD_TITLE );
+		$entries            = static::gen_field_key( static::FIELD_ENTRIES );
+		$entry_title        = static::gen_field_key( static::FIELD_ENTRY_TITLE );
+		$entry_lines        = static::gen_field_key( static::FIELD_ENTRY_LINES );
+		$line               = static::gen_field_key( static::FIELD_LINE );
+		$attr    = static::gen_field_key( static::FIELD_ATTR );
+
+		return [
+			'title'           => 'Address',
+			'fields'          => [
+				static::FIELD_TITLE   => [
+					'key'               => $title,
+					'name'              => static::FIELD_TITLE,
+					'label'             => 'Title',
+					'type'              => 'text',
+				],
+				static::FIELD_ENTRIES => [
+					'key'               => $entries,
+					'name'              => static::FIELD_ENTRIES,
+					'label'             => 'Entries',
+					'type'              => 'repeater',
+					'required'          => 1,
+					'layout'            => 'block',
+					'sub_fields'        => [
+						static::FIELD_ENTRY_TITLE     => [
+							'key'                       => $entry_title,
+							'name'                      => static::FIELD_ENTRY_TITLE,
+							'label'                     => 'Title',
+							'type'                      => 'text',
+						],
+						static::FIELD_ENTRY_LINES     => [
+							'key'                       => $entry_lines,
+							'name'                      => static::FIELD_ENTRY_LINES,
+							'label'                     => 'Line entries',
+							'type'                      => 'repeater',
+							'layout'                    => 'block',
+							'sub_fields'                => [
+								static::FIELD_LINE   => [
+									'key'              => $line,
+									'name'             => static::FIELD_LINE,
+									'label'            => 'Line',
+									'type'             => 'textarea',
+								],
+							],
+						],
+					]
+				],
+				static::FIELD_ATTR    => DOM_Attr::clone( [
+					'key'   => $attr,
+					'name'  => static::FIELD_ATTR,
+					'label' => '',
+				] ),
+			],
+		];
+	}
+
+	public static function get_block_args(): array {
+		return array_merge( parent::get_block_args(), [
+			'name'         => static::get_key(),
+			'title'        => 'Address',
+			'post_types'   => [ 'page', ],
+		] );
+	}
+
+	public static function render_block( $block, $content = '', $is_preview = false, $post_id = 0 ): void {
+		$title = static::get_val( static::FIELD_TITLE ) ?: 'Block Title';
+		$entries = static::get_val( static::FIELD_ENTRIES ) ?: [];
+
+		ob_start();
+		?>
+			<div>
+				<h2 class="font-bold mt-px60 md:mt-px80 lg:mt-px90 text-px28 leading-px34 md:text-px32 md:leading-px42 lg:text-px36 lg:leading-px40">
+					<?php echo esc_html( @$title ); ?>
+				</h2>
+				<?php if ( ! empty( $entries ) ) { ?>
+					<div class="flex flex-col md:flex-row address-container mt-px20 lg:mt-px30">
+						<?php foreach( $entries as $entry ) { ?>
+							<div class="mb-px28 md:mb-0 md:mr-px40 lg:mr-px50 md:mt-px40 lg:mt-0">
+								<h3 class="font-bold mb-px6 lg:mb-px12 text-px20 leading-px30 md:leading-px26 lg:text-px24 lg:leading-px30"><?= esc_html( @$entry[ static::FIELD_ENTRY_TITLE ] ) ?></h3>
+								<?php foreach ( @$entry[ 'entry_lines' ] as $line ) { ?>
+									<div class="font-normal text-px18 leading-px24 lg:text-px22 lg:leading-px28"><?php echo esc_html( $line[ static::FIELD_LINE ] ); ?></div>
+								<?php } ?>
+							</div>
+						<?php } ?>
+					</div>
+				<?php } ?>
+			</div>
+		<?php echo ob_get_clean();
+	}
+}
