384d4448ec3d27947f87a1501ec743915af859ec
Rachel M. Arcangel <rachel@codeandtheory.com>
2021-05-11 19:39:24 +0800

    KTTL-297 - Nav Logo Color Fix (#511)
    KTTL-297 - Change default background color to indigo

--- theme/inc/Helper/Post_Header.php
+++ theme/inc/Helper/Post_Header.php
@@ -10,47 +10,68 @@ use TrevorWP\Util\Tools;
  */
 class Post_Header {
 	/* Types */
-	const TYPE_SPLIT = 'split';
-	const TYPE_FULL = 'full';
+	const TYPE_SPLIT      = 'split';
+	const TYPE_FULL       = 'full';
 	const TYPE_HORIZONTAL = 'horizontal';
-	const TYPE_SQUARE = 'square';
-	const TYPE_TEXT_ONLY = 'text_only';
-	const ALL_TYPES = [
-			self::TYPE_SPLIT,
-			self::TYPE_FULL,
-			self::TYPE_HORIZONTAL,
-			self::TYPE_SQUARE,
-			self::TYPE_TEXT_ONLY,
+	const TYPE_SQUARE     = 'square';
+	const TYPE_TEXT_ONLY  = 'text_only';
+	const ALL_TYPES       = [
+		self::TYPE_SPLIT,
+		self::TYPE_FULL,
+		self::TYPE_HORIZONTAL,
+		self::TYPE_SQUARE,
+		self::TYPE_TEXT_ONLY,
 	];
 
 	/* Colors */
-	const CLR_WHITE = 'white';
-	const CLR_LIGHT_GRAY = 'gray-light';
-	const CLR_INDIGO = 'indigo';
-	const CLR_BLUE_GREEN = 'blue_green';
-	const BG_COLORS = [
-			self::CLR_LIGHT_GRAY => [ 'name' => 'Light Gray', 'color' => '#F3F3F7' ],
-			self::CLR_INDIGO     => [ 'name' => 'Indigo', 'color' => '#101066' ],
-			self::CLR_BLUE_GREEN => [ 'name' => 'Blue Green', 'color' => '#005E67' ],
+	const CLR_WHITE        = 'white';
+	const CLR_LIGHT_GRAY   = 'gray-light';
+	const CLR_INDIGO       = 'indigo';
+	const CLR_BLUE_GREEN   = 'blue_green';
+	const BG_COLORS        = [
+		self::CLR_LIGHT_GRAY => [
+			'name'  => 'Light Gray',
+			'color' => '#F3F3F7',
+		],
+		self::CLR_INDIGO     => [
+			'name'  => 'Indigo',
+			'color' => '#101066',
+		],
+		self::CLR_BLUE_GREEN => [
+			'name'  => 'Blue Green',
+			'color' => '#005E67',
+		],
 	];
 	const BG_CLR_2_TXT_CLR = [
-			self::CLR_LIGHT_GRAY => self::CLR_INDIGO,
-			self::CLR_INDIGO     => self::CLR_WHITE,
-			self::CLR_BLUE_GREEN => self::CLR_WHITE,
+		self::CLR_LIGHT_GRAY => self::CLR_INDIGO,
+		self::CLR_INDIGO     => self::CLR_WHITE,
+		self::CLR_BLUE_GREEN => self::CLR_WHITE,
 	];
 
 	/* Settings */
 	const SETTINGS = [
-			self::TYPE_SPLIT      => [ 'name' => 'Split' ],
-			self::TYPE_FULL       => [ 'name' => 'Full Bleed', 'validate' => [ 'image-horizontal' ] ],
-			self::TYPE_HORIZONTAL => [ 'name' => 'Horizontal', 'validate' => [ 'image-horizontal' ] ],
-			self::TYPE_SQUARE     => [ 'name' => 'Square', 'validate' => [ 'image-square' ] ],
-			self::TYPE_TEXT_ONLY  => [ 'name' => 'Text Only', 'supports' => [ 'bg-color' ] ],
+		self::TYPE_SPLIT      => [ 'name' => 'Split' ],
+		self::TYPE_FULL       => [
+			'name'     => 'Full Bleed',
+			'validate' => [ 'image-horizontal' ],
+		],
+		self::TYPE_HORIZONTAL => [
+			'name'     => 'Horizontal',
+			'validate' => [ 'image-horizontal' ],
+		],
+		self::TYPE_SQUARE     => [
+			'name'     => 'Square',
+			'validate' => [ 'image-square' ],
+		],
+		self::TYPE_TEXT_ONLY  => [
+			'name'     => 'Text Only',
+			'supports' => [ 'bg-color' ],
+		],
 	];
 
 	/* Defaults */
-	const DEFAULT_TYPE = self::TYPE_TEXT_ONLY;
-	const DEFAULT_BG_COLOR = self::CLR_LIGHT_GRAY;
+	const DEFAULT_TYPE     = self::TYPE_TEXT_ONLY;
+	const DEFAULT_BG_COLOR = self::CLR_INDIGO;
 
 	/**
 	 * Renders the post header.
@@ -61,33 +82,35 @@ class Post_Header {
 	 * @return string
 	 */
 	public static function render( \WP_Post $post, array $options = [] ): string {
-		$type = $options['type'] = self::get_header_type( $post );
-		$cls  = [ 'post-header', "type-${type}" ];
+		$options['type'] = self::get_header_type( $post );
+		$type            = $options['type'];
+		$cls             = [ 'post-header', "type-${type}" ];
 
 		# BG Color
 		if ( self::supports_bg_color( $post ) ) {
 			[ $bg_color, $txt_color ] = self::get_bg_color( $post );
-			$cls[] = "bg-{$bg_color}";
-			$cls[] = "text-{$txt_color}";
+			$cls[]                    = "bg-{$bg_color}";
+			$cls[]                    = "text-{$txt_color}";
 		} else {
 			$cls[] = 'text-white';
 		}
 
 		$thumb_wrap_attrs = [];
-		if ( $type == self::TYPE_SQUARE ) {
+		if ( self::TYPE_SQUARE == $type ) {
 			$thumb_wrap_attrs['data-aspectRatio'] = '1:1';
 		}
 
-		ob_start(); ?>
+		ob_start() ?>
 		<div class="<?= esc_attr( implode( ' ', $cls ) ) ?>">
 			<div class="container post-header-inner">
 				<?= self::_render_content_area( $post, $options ) ?>
 				<div class="thumbnail-wrap" <?= Tools::flat_attr( $thumb_wrap_attrs ) ?>>
-					<?= self::_render_thumbnail( $post, $options ); ?>
+					<?= self::_render_thumbnail( $post, $options ) ?>
 				</div>
 			</div>
 		</div>
-		<?php return ob_get_clean();
+		<?php
+		return ob_get_clean();
 	}
 
 	/**
@@ -134,22 +157,25 @@ class Post_Header {
 	 * @return string
 	 */
 	protected static function _render_content_area( \WP_Post $post, array &$options = [] ): string {
-		$title_top = $title_btm = $external_url = null;
-		$hide_tags = false;
+		$external_url = null;
+		$title_btm    = null;
+		$title_top    = null;
+		$hide_tags    = false;
 
 		# By post type
-		if ( $post->post_type == CPT\RC\Guide::POST_TYPE ) {
+		if ( CPT\RC\Guide::POST_TYPE == $post->post_type ) {
 			$title_top = 'Guide';
 		} elseif ( in_array( $post->post_type, Main::BLOG_POST_TYPES ) ) {
 			$title_top = 'Blog';
-		} elseif ( $post->post_type == CPT\RC\External::POST_TYPE ) {
+		} elseif ( CPT\RC\External::POST_TYPE == $post->post_type ) {
 			$hide_tags    = true;
 			$external_url = CPT\RC\External::obj_get_url( $post->ID );
-		} elseif ( $post->post_type == CPT\Get_Involved\Bill::POST_TYPE ) {
+		} elseif ( CPT\Get_Involved\Bill::POST_TYPE == $post->post_type ) {
 			$title_top = Meta\Post::get_bill_id( $post->ID );
 		}
 
-		if ( empty( $title_top ) && ! empty( $main_cat = Meta\Post::get_main_category( $post ) ) ) {
+		$main_cat = Meta\Post::get_main_category( $post );
+		if ( empty( $title_top ) && ! empty( $main_cat ) ) {
 			$title_top = $main_cat->name;
 		}
 
@@ -162,42 +188,51 @@ class Post_Header {
 		$mid_row = [];
 
 		## Article Length
-		if ( ! empty( $len_ind = Content_Length::post( $post ) ) ) {
-			$mid_row[] = '<div class="length-indicator"> ' . ( $post->post_type == CPT\RC\Guide::POST_TYPE ? 'Guide' : 'Article' ) . ' Length: ' . esc_html( $len_ind ) . "</div>";
+		$len_ind = Content_Length::post( $post );
+		if ( ! empty( $len_ind ) ) {
+			$mid_row[] = '<div class="length-indicator"> ' . ( CPT\RC\Guide::POST_TYPE == $post->post_type ? 'Guide' : 'Article' ) . ' Length: ' . esc_html( $len_ind ) . '</div>';
 		}
 
 		## Date Box
 		if ( Meta\Post::can_show_date_box( $post->ID ) ) {
-			$dateTime = new \DateTime( $post->post_date );
-			ob_start(); ?>
+			$date_time = new \DateTime( $post->post_date );
+			ob_start();
+			?>
 
 			<div class="date-box">
 				<time class="font-semibold text-px14 leading-px18"
-					  datetime="<?= $post->post_date ?>"><?= $dateTime->format( 'M. j, Y' ) ?></time>
+					  datetime="<?= $post->post_date ?>"><?= $date_time->format( 'M. j, Y' ) ?></time>
 			</div>
 
-			<?php $mid_row[] = ob_get_clean();
+			<?php
+			$mid_row[] = ob_get_clean();
 		}
 
 		## Sharing Box
 		if ( Meta\Post::can_show_share_box( $post->ID ) ) {
-			ob_start(); ?>
+			ob_start();
+
+			$facebook_share_query = http_build_query( [ 'u' => get_permalink() ] );
+			$twitter_share_query  = http_build_query(
+				[
+					'text' => get_the_title( $post ),
+					'url'  => get_permalink( $post ),
+				]
+			);
+			?>
 			<div class="sharing-box">
 				<a target="_blank"
 				   rel="noopener noreferrer nofollow"
 				   class="post-social-share-btn"
 				   data-type="facebook"
-				   href="https://www.facebook.com/sharer.php?<?= http_build_query( [ 'u' => get_permalink() ] ) ?>">
+				   href="https://www.facebook.com/sharer.php?<?= $facebook_share_query ?>">
 					<i class="share-icon trevor-ti-facebook hover:text-melrose"></i>
 				</a>
 				<a target="_blank"
 				   rel="noopener noreferrer nofollow"
 				   class="post-social-share-btn"
 				   data-type="twitter"
-				   href="https://twitter.com/share?<?= http_build_query( [
-						   'text' => get_the_title( $post ),
-						   'url'  => get_permalink( $post )
-				   ] ) ?>">
+				   href="https://twitter.com/share?<?= $twitter_share_query ?>">
 					<i class="share-icon trevor-ti-twitter hover:text-melrose"></i>
 				</a>
 				<span>
@@ -211,20 +246,20 @@ class Post_Header {
 								<table class="post-share-more-list">
 									<tbody>
 									<tr data-row="facebook">
-									    <td><i class="trevor-ti-facebook-alt"></i></td>
-									    <td>Facebook</td>
+										<td><i class="trevor-ti-facebook-alt"></i></td>
+										<td>Facebook</td>
 									</tr>
 									<tr data-row="twitter">
-									    <td><i class="trevor-ti-twitter"></i></td>
-									    <td>Twitter</td>
+										<td><i class="trevor-ti-twitter"></i></td>
+										<td>Twitter</td>
 									</tr>
 									<tr data-row="clipboard">
-									    <td><i class="trevor-ti-link"></i></td>
-									    <td>Copy Link</td>
+										<td><i class="trevor-ti-link"></i></td>
+										<td>Copy Link</td>
 									</tr>
 									<tr data-row="email">
-									    <td><i class="trevor-ti-mail"></i></td>
-									    <td>Email</td>
+										<td><i class="trevor-ti-mail"></i></td>
+										<td>Email</td>
 									</tr>
 									</tbody>
 								</table>
@@ -233,15 +268,20 @@ class Post_Header {
 					</span>
 				</span>
 			</div>
-			<?php $mid_row[] = ob_get_clean();
+			<?php
+			$mid_row[] = ob_get_clean();
 		}
 
 		# Tags
-		$tags = $hide_tags ? [] : Taxonomy::get_post_tags_distinctive( $post, [
+		$tags = $hide_tags ? [] : Taxonomy::get_post_tags_distinctive(
+			$post,
+			[
 				'filter_count_1' => false,
-		] );
+			]
+		);
 
-		ob_start(); ?>
+		ob_start();
+		?>
 		<div class="post-header-content">
 			<?php if ( $title_top ) { ?>
 				<div class="title-top"><?= $title_top ?></div>
@@ -251,7 +291,10 @@ class Post_Header {
 				<div class="title-btm"><?= $title_btm ?></div>
 			<?php } ?>
 
-			<?php if ( $mid_row_count = count( $mid_row ) ) { ?>
+			<?php
+			$mid_row_count = count( $mid_row );
+			if ( $mid_row_count ) {
+				?>
 				<div class="mid-row">
 					<?php foreach ( $mid_row as $idx => $col ) { ?>
 						<div class="mid-row-col">
@@ -281,7 +324,8 @@ class Post_Header {
 				</div>
 			<?php } ?>
 		</div>
-		<?php return ob_get_clean();
+		<?php
+		return ob_get_clean();
 	}
 
 	/**
