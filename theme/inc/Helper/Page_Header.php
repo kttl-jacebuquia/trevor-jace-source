b43b792f4eede508c4b3325822aa36b989b69595
ct-jacebuquia <74394071+ct-jacebuquia@users.noreply.github.com>
2021-05-24 18:55:34 +0800

    KTTL-316 - Desktop Page Header / H1 and description styling update (#570)
    Co-authored-by: Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>

--- theme/inc/Helper/Page_Header.php
+++ theme/inc/Helper/Page_Header.php
@@ -49,7 +49,7 @@ class Page_Header {
 				'flex flex-col font-bold',
 				'text-px32 leading-px40 mb-px20',
 				'md:text-px36 md:leading-px42 md:mb-5 md:inline-block md:mb-px20',
-				'xl:mb-7 xl:text-px56 xl:leading-px66 xl:tracking-em005 xl:mb-px28',
+				'xl:mb-7 xl:text-px46 xl:leading-px56 xl:tracking-em005 xl:mb-px28',
 			)
 		);
 
