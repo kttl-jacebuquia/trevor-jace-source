3b3ddbab9975e9bf360b6f17d1bc354097432859
Ralph Allen Valencia <ralph.allen.valencia@codeandtheory.com>
2021-06-11 10:59:28 +0800

    KTTL-345 - Create Guiding Principles Gutenberg Block (#574)

---
+++ theme/inc/ACF/Field_Group/Guiding_Principles.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace TrevorWP\Theme\ACF\Field_Group;
+
+class Guiding_Principles extends A_Field_Group implements I_Block, I_Renderable {
+
+	const FIELD_TITLE                 = 'title';
+	const FIELD_DESCRIPTION           = 'description';
+	const FIELD_PRINCIPLE_ENTRIES     = 'principle_entries';
+	const FIELD_PRINCIPLE_ENTRY_TITLE = 'principle_entry_title';
+	const FIELD_FILE                  = 'file';
+	const FIELD_BUTTON_TEXT           = 'button_text';
+	const FIELD_IMAGE                 = 'image';
+
+	/**
+	 * @inheritDoc
+	 */
+	protected static function prepare_register_args(): array {
+		$title                 = static::gen_field_key( static::FIELD_TITLE );
+		$description           = static::gen_field_key( static::FIELD_DESCRIPTION );
+		$principle_entries     = static::gen_field_key( static::FIELD_PRINCIPLE_ENTRIES );
+		$principle_entry_title = static::gen_field_key( static::FIELD_PRINCIPLE_ENTRY_TITLE );
+		$file                  = static::gen_field_key( static::FIELD_FILE );
+		$button_text           = static::gen_field_key( static::FIELD_BUTTON_TEXT );
+		$image                 = static::gen_field_key( static::FIELD_IMAGE );
+
+		return array(
+			'title'  => 'Guiding Principles',
+			'fields' => array(
+				static::FIELD_TITLE             => array(
+					'key'      => $title,
+					'name'     => static::FIELD_TITLE,
+					'label'    => 'Title',
+					'type'     => 'text',
+					'required' => 1,
+					'wrapper'  => array(
+						'width' => '50%',
+					),
+				),
+				static::FIELD_DESCRIPTION       => array(
+					'key'     => $description,
+					'name'    => static::FIELD_DESCRIPTION,
+					'label'   => 'Description',
+					'type'    => 'textarea',
+					'wrapper' => array(
+						'width' => '50%',
+					),
+				),
+				static::FIELD_PRINCIPLE_ENTRIES => array(
+					'key'        => $principle_entries,
+					'name'       => static::FIELD_PRINCIPLE_ENTRIES,
+					'label'      => 'Principle Entries',
+					'type'       => 'repeater',
+					'layout'     => 'block',
+					'sub_fields' => array(
+						static::FIELD_PRINCIPLE_ENTRY_TITLE => array(
+							'key'      => $principle_entry_title,
+							'name'     => static::FIELD_PRINCIPLE_ENTRY_TITLE,
+							'label'    => 'Title',
+							'type'     => 'text',
+							'required' => 1,
+						),
+					),
+					'max'        => 6,
+				),
+				static::FIELD_FILE              => array(
+					'key'           => $file,
+					'name'          => static::FIELD_FILE,
+					'label'         => 'File',
+					'type'          => 'file',
+					'return_format' => 'array',
+					'library'       => 'all',
+					'required'      => 1,
+					'wrapper'       => array(
+						'width' => '50%',
+					),
+				),
+				static::FIELD_BUTTON_TEXT       => array(
+					'key'      => $button_text,
+					'name'     => static::FIELD_BUTTON_TEXT,
+					'label'    => 'Button Text',
+					'type'     => 'text',
+					'required' => 1,
+					'wrapper'  => array(
+						'width' => '50%',
+					),
+				),
+				static::FIELD_IMAGE             => array(
+					'key'           => $image,
+					'name'          => static::FIELD_IMAGE,
+					'label'         => 'Image',
+					'type'          => 'image',
+					'return_format' => 'array',
+					'preview_size'  => 'thumbnail',
+					'library'       => 'all',
+				),
+			),
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function get_block_args(): array {
+		return array_merge(
+			parent::get_block_args(),
+			array(
+				'name'       => static::get_key(),
+				'title'      => 'Guiding Principles',
+				'post_types' => array( 'page' ),
+			)
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function render( $post = false, array $data = null, array $options = array() ): ?string {
+		$title             = static::get_val( static::FIELD_TITLE );
+		$description       = static::get_val( static::FIELD_DESCRIPTION );
+		$principle_entries = static::get_val( static::FIELD_PRINCIPLE_ENTRIES );
+		$file              = static::get_val( static::FIELD_FILE );
+		$button_text       = static::get_val( static::FIELD_BUTTON_TEXT );
+		$image             = static::get_val( static::FIELD_IMAGE );
+
+		ob_start();
+		// Next Step: FE
+		?>
+		<div class="container mx-auto">
+			<h3><?php echo esc_html( $title ); ?></h3>
+			<?php if ( ! empty( $description ) ) : ?>
+				<p><?php echo esc_html( $description ); ?></p>
+			<?php endif; ?>
+			<div>
+				<?php if ( ! empty( $principle_entries ) ) : ?>
+					<div>
+						<?php foreach ( $principle_entries as $entry ) : ?>
+							<div>
+								<?php if ( ! empty( $entry[ static::FIELD_PRINCIPLE_ENTRY_TITLE ] ) ) : ?>
+									<h2><?php echo esc_html( $entry[ static::FIELD_PRINCIPLE_ENTRY_TITLE ] ); ?></h2>
+								<?php endif; ?>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+				<?php if ( ! empty( $file['url'] && ! empty( $button_text ) ) ) : ?>
+					<a href="<?php echo esc_url( $file['url'] ); ?>" download><?php echo esc_html( $button_text ); ?></a>
+				<?php endif; ?>
+				<?php if ( ! empty( $image['url'] ) ) : ?>
+					<div>
+						<img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo ( ! empty( $image['alt'] ) ) ? esc_attr( $image['alt'] ) : esc_attr( $title ); ?>">
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+		return ob_get_clean();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function render_block( $block, $content = '', $is_preview = false, $post_id = 0 ): void {
+		echo static::render( $post_id, null, compact( 'is_preview' ) );
+	}
+}
