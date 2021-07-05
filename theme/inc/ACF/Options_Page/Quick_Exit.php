130a3678cba929b34c73f6063dbbfd44fa4a71c4
ct-jacebuquia <74394071+ct-jacebuquia@users.noreply.github.com>
2021-07-05 23:55:18 +0800

    ✨ KTTL-543 - Quick Exit close link (#641)
    Co-authored-by: Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>

--- theme/inc/ACF/Options_Page/Quick_Exit.php
+++ theme/inc/ACF/Options_Page/Quick_Exit.php
@@ -65,7 +65,7 @@ class Quick_Exit extends A_Options_Page {
 				<p class="quick-exit-modal__description quick-exit-modal__description--desktop">
 					<?php echo esc_html( $description_desktop ); ?>
 				</p>
-				<button aria-label="click here to close modal" class="quick-exit-modal__cta js-modal-close"><?php echo esc_html( $link_text ); ?></button>
+				<a href="#" name="Quick exit close" aria-label="click here to close this modal" class="quick-exit-modal__cta js-modal-close"><?php echo esc_html( $link_text ); ?></a>
 			</div>
 		<?php
 		return ob_get_clean();
