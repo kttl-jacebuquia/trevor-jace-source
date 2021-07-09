a62f3b99430babde78ba74a54fb2208618cbce61
Ralph Allen Valencia <ralph.allen.valencia@codeandtheory.com>
2021-07-09 12:05:52 +0800

    ✨ KTTL-470 - Add Crisis Services Image + Text (#658)
    Co-authored-by: Joshua Cerbito <josh@codeandtheory.com>

--- theme/inc/ACF/ACF.php
+++ theme/inc/ACF/ACF.php
@@ -53,6 +53,7 @@ class ACF {
 		Field_Group\File_Download::class,
 		Field_Group\Tier::class,
 		Field_Group\CTA_Block::class,
+		Field_Group\Crisis_Services_Text_Block::class,
 		Field_Group\Messaging_Block::class,
 		// Page Specific
 		Field_Group\Page_Header::class,
---
+++ theme/inc/ACF/Field_Group/Crisis_Services_Text_Block.php
@@ -0,0 +1,108 @@
+<?php namespace TrevorWP\Theme\ACF\Field_Group;
+
+class Crisis_Services_Text_Block extends A_Field_Group implements I_Block, I_Renderable {
+	const FIELD_TITLE       = 'title';
+	const FIELD_DESCRIPTION = 'description';
+	const FIELD_BUTTON      = 'button';
+	const FIELD_IMAGE       = 'image';
+
+	/**
+	 * @inheritDoc
+	 */
+	protected static function prepare_register_args(): array {
+		$title       = static::gen_field_key( static::FIELD_TITLE );
+		$description = static::gen_field_key( static::FIELD_DESCRIPTION );
+		$button      = static::gen_field_key( static::FIELD_BUTTON );
+		$image       = static::gen_field_key( static::FIELD_IMAGE );
+
+		return array(
+			'title'  => 'Crisis Services Image + Text',
+			'fields' => array(
+				static::FIELD_TITLE       => array(
+					'key'      => $title,
+					'name'     => static::FIELD_TITLE,
+					'label'    => 'Title',
+					'type'     => 'text',
+					'required' => 1,
+				),
+				static::FIELD_DESCRIPTION => array(
+					'key'   => $description,
+					'name'  => static::FIELD_DESCRIPTION,
+					'label' => 'Description',
+					'type'  => 'textarea',
+				),
+				static::FIELD_BUTTON      => array(
+					'key'   => $button,
+					'name'  => static::FIELD_BUTTON,
+					'label' => 'Button',
+					'type'  => 'link',
+				),
+				static::FIELD_IMAGE       => array(
+					'key'           => $image,
+					'name'          => static::FIELD_IMAGE,
+					'label'         => 'Image',
+					'type'          => 'image',
+					'required'      => 1,
+					'return_format' => 'array',
+					'preview_size'  => 'thumbnail',
+					'library'       => 'all',
+				),
+			),
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function get_block_args(): array {
+		return array_merge(
+			parent::get_block_args(),
+			array(
+				'name'       => static::get_key(),
+				'title'      => 'Crisis Services Image + Text',
+				'post_types' => array( 'page' ),
+			)
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function render( $post = false, array $data = null, array $options = array() ): ?string {
+		$title       = static::get_val( static::FIELD_TITLE );
+		$description = static::get_val( static::FIELD_DESCRIPTION );
+		$button      = static::get_val( static::FIELD_BUTTON );
+		$image       = static::get_val( static::FIELD_IMAGE );
+
+		ob_start();
+		?>
+		<div class="container mx-auto">
+			<?php if ( ! empty( $title ) ) : ?>
+				<h3><?php echo esc_html( $title ); ?></h3>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $description ) ) : ?>
+				<p><?php echo esc_html( $description ); ?></p>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $button['url'] ) ) : ?>
+				<a href="<?php echo esc_url( $button['url'] ); ?>" target="<?php echo esc_attr( $button['target'] ); ?>"><?php echo esc_html( $button['title'] ); ?></a>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $image['url'] ) ) : ?>
+				<div class="text-center mt-px50 block">
+					<img src="<?php echo esc_url( $image['url'] ); ?>" class="block mx-auto" alt="<?php echo ( ! empty( $image['alt'] ) ) ? esc_attr( $image['alt'] ) : esc_attr( $title ); ?>">
+				</div>
+			<?php endif; ?>
+		</div>
+		<?php
+		return ob_get_clean();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function render_block( $block, $content = '', $is_preview = false, $post_id = 0 ): void {
+		echo static::render( $post_id, null, compact( 'is_preview' ) );
+	}
+}
