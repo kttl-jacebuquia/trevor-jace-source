4bf353823af772c755c63f2be7e4330f0c49c20e
Ralph Allen Valencia <ralph.allen.valencia@codeandtheory.com>
2021-08-03 18:25:11 +0800

    ✨ KTTL-806: Add 404 option page (#718)

--- theme/404.php
+++ theme/404.php
@@ -1,7 +1,30 @@
+<?php
+
+use TrevorWP\Theme\ACF\Options_Page\Four_O_Four;
+
+$four_o_four_data = Four_O_Four::get_four_o_four();
+?>
 <?php get_header(); ?>
 
 <main id="site-content" class="site-content" role="main">
-	404
+	<?php if ( ! empty( $four_o_four_data['headline'] ) ) : ?>
+		<h1><?php echo esc_html( $four_o_four_data['headline'] ); ?></h1>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $four_o_four_data['description'] ) ) : ?>
+		<p><?php echo esc_html( $four_o_four_data['description'] ); ?></p>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $four_o_four_data['link']['url'] ) ) : ?>
+		<a href="<?php echo esc_url( $four_o_four_data['link']['url'] ); ?>" target="<?php echo esc_attr( $four_o_four_data['link']['target'] ); ?>"><?php echo esc_html( $four_o_four_data['link']['title'] ); ?></a>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $four_o_four_data['image']['url'] ) ) : ?>
+		<img
+			src="<?php echo esc_url( $four_o_four_data['image']['url'] ); ?>"
+			alt="<?php echo esc_attr( $four_o_four_data['image']['alt'] ); ?>"
+		/>
+	<?php endif; ?>
 </main> <!-- #site-content -->
 
 <?php get_footer(); ?>
---
+++ theme/inc/ACF/Options_Page/Four_O_Four.php
@@ -0,0 +1,89 @@
+<?php namespace TrevorWP\Theme\ACF\Options_Page;
+
+class Four_O_Four extends A_Options_Page {
+	const FIELD_HEADLINE    = 'four_o_four_headline';
+	const FIELD_DESCRIPTION = 'four_o_four_description';
+	const FIELD_LINK        = 'four_o_four_link';
+	const FIELD_IMAGE       = 'four_o_four_image';
+
+	/** @inheritDoc */
+	protected static function prepare_page_register_args(): array {
+		return array_merge(
+			parent::prepare_page_register_args(),
+			array(
+				'parent_slug' => 'general-settings',
+				'page_title' => '404 Settings',
+				'menu_title' => '404',
+			)
+		);
+	}
+
+	/** @inheritDoc */
+	protected static function prepare_register_args(): array {
+		return array_merge(
+			parent::prepare_register_args(),
+			array(
+				'title' => '404 Settings',
+			),
+		);
+	}
+
+	/** @inheritDoc */
+	protected static function prepare_fields(): array {
+		$headline    = static::gen_field_key( static::FIELD_HEADLINE );
+		$description = static::gen_field_key( static::FIELD_DESCRIPTION );
+		$link        = static::gen_field_key( static::FIELD_LINK );
+		$image       = static::gen_field_key( static::FIELD_IMAGE );
+
+		return array_merge(
+			array(
+				static::FIELD_HEADLINE    => array(
+					'key'           => $headline,
+					'name'          => static::FIELD_HEADLINE,
+					'label'         => 'Headline',
+					'type'          => 'text',
+					'required'      => true,
+					'default_value' => 'Oops!',
+				),
+				static::FIELD_DESCRIPTION => array(
+					'key'           => $description,
+					'name'          => static::FIELD_DESCRIPTION,
+					'label'         => 'Description',
+					'type'          => 'text',
+					'default_value' => 'The page you\'re looking for doesn\'t exist',
+				),
+				static::FIELD_LINK        => array(
+					'key'           => $link,
+					'name'          => static::FIELD_LINK,
+					'label'         => 'Link',
+					'type'          => 'link',
+					'return_format' => 'array',
+				),
+				static::FIELD_IMAGE       => array(
+					'key'           => $image,
+					'name'          => static::FIELD_IMAGE,
+					'label'         => 'Image',
+					'type'          => 'image',
+					'required'      => 1,
+					'return_format' => 'array',
+					'preview_size'  => 'thumbnail',
+					'library'       => 'all',
+				),
+			),
+		);
+	}
+
+	/**
+	 * Gets all 404 values
+	 */
+	public static function get_four_o_four() {
+		$data = array(
+			'headline'    => static::get_option( static::FIELD_HEADLINE ),
+			'description' => static::get_option( static::FIELD_DESCRIPTION ),
+			'link'        => static::get_option( static::FIELD_LINK ),
+			'image'       => static::get_option( static::FIELD_IMAGE ),
+		);
+
+		return $data;
+	}
+}
