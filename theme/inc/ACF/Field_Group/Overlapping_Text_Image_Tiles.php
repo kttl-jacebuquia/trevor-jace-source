aee0c81bcf7d74e18d2c6c3821347b03feba4ae3
Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>
2021-08-18 17:10:48 +0800

    KTTL-478 - [FE] Component — TrevorSpace text + image block

--- theme/inc/ACF/Field_Group/Overlapping_Text_Image_Tiles.php
+++ theme/inc/ACF/Field_Group/Overlapping_Text_Image_Tiles.php
@@ -1,10 +1,12 @@
 <?php namespace TrevorWP\Theme\ACF\Field_Group;
 
-class Crisis_Services_Text_Block extends A_Field_Group implements I_Block, I_Renderable {
+class Overlapping_Text_Image_Tiles extends A_Field_Group implements I_Block, I_Renderable {
 	const FIELD_TITLE       = 'title';
 	const FIELD_DESCRIPTION = 'description';
 	const FIELD_BUTTON      = 'button';
 	const FIELD_IMAGE       = 'image';
+	const FIELD_STYLES      = 'styles';
+	const FIELD_LAYOUT      = 'layout';
 
 	/**
 	 * @inheritDoc
@@ -14,10 +16,36 @@ class Crisis_Services_Text_Block extends A_Field_Group implements I_Block, I_Ren
 		$description = static::gen_field_key( static::FIELD_DESCRIPTION );
 		$button      = static::gen_field_key( static::FIELD_BUTTON );
 		$image       = static::gen_field_key( static::FIELD_IMAGE );
+		$styles      = static::gen_field_key( static::FIELD_STYLES );
+		$layout      = static::gen_field_key( static::FIELD_LAYOUT );
 
 		return array(
-			'title'  => 'Crisis Services Image + Text',
+			'title'  => 'Overlapping Text and Image Tiles',
 			'fields' => array(
+				static::FIELD_STYLES      => Block_Styles::clone(
+					array(
+						'key'  => $styles,
+						'name' => static::FIELD_STYLES,
+					),
+				),
+				static::FIELD_LAYOUT      => array(
+					'key'           => $layout,
+					'name'          => static::FIELD_LAYOUT,
+					'label'         => 'Layout',
+					'type'          => 'radio',
+					'layout'        => 'vertical',
+					'default_value' => 'image-on-left',
+					'choices'       => array(
+						'image-on-left'  => 'Image on Left',
+						'image-on-right' => 'Image on Right',
+					),
+				),
+				static::FIELD_STYLES      => Block_Styles::clone(
+					array(
+						'key'  => $styles,
+						'name' => static::FIELD_STYLES,
+					),
+				),
 				static::FIELD_TITLE       => array(
 					'key'      => $title,
 					'name'     => static::FIELD_TITLE,
@@ -59,7 +87,7 @@ class Crisis_Services_Text_Block extends A_Field_Group implements I_Block, I_Ren
 			parent::get_block_args(),
 			array(
 				'name'       => static::get_key(),
-				'title'      => 'Crisis Services Image + Text',
+				'title'      => 'Overlapping Text and Image Tiles',
 				'post_types' => array( 'page' ),
 			)
 		);
@@ -73,26 +101,44 @@ class Crisis_Services_Text_Block extends A_Field_Group implements I_Block, I_Ren
 		$description = static::get_val( static::FIELD_DESCRIPTION );
 		$button      = static::get_val( static::FIELD_BUTTON );
 		$image       = static::get_val( static::FIELD_IMAGE );
+		$styles      = static::get_val( static::FIELD_STYLES );
+		$layout      = static::get_val( static::FIELD_LAYOUT );
+
+		list(
+			$bg_color,
+			$text_color,
+		) = array_values( $styles );
+
+		$container_class = array(
+			'overlapping-text-image-tiles',
+			'overlapping-text-image-tiles--' . $layout,
+		);
+
+		$box_class = array(
+			'overlapping-text-image-tiles__box',
+			'bg-' . $bg_color,
+			'text-' . $text_color,
+		);
 
 		ob_start();
 		?>
-		<div class="crisis-services-image-text">
-			<div class="crisis-services-image-text__container">
-				<div class="crisis-services-image-text__box">
+		<div <?php echo static::render_attrs( $container_class ); ?>>
+			<div class="overlapping-text-image-tiles__container">
+				<div <?php echo static::render_attrs( $box_class ); ?>>
 					<?php if ( ! empty( $title ) ) : ?>
-						<h3 class="crisis-services-image-text__heading"><?php echo $title; ?></h3>
+						<h3 class="overlapping-text-image-tiles__heading"><?php echo $title; ?></h3>
 					<?php endif; ?>
 
 					<?php if ( ! empty( $description ) ) : ?>
-						<p class="crisis-services-image-text__description"><?php echo esc_html( $description ); ?></p>
+						<p class="overlapping-text-image-tiles__description"><?php echo esc_html( $description ); ?></p>
 					<?php endif; ?>
 
 					<?php if ( ! empty( $button['url'] ) ) : ?>
-						<a class="crisis-services-image-text__cta" href="<?php echo esc_url( $button['url'] ); ?>" target="<?php echo esc_attr( $button['target'] ); ?>"><?php echo esc_html( $button['title'] ); ?></a>
+						<a class="overlapping-text-image-tiles__cta" href="<?php echo esc_url( $button['url'] ); ?>" target="<?php echo esc_attr( $button['target'] ); ?>"><?php echo esc_html( $button['title'] ); ?></a>
 					<?php endif; ?>
 				</div>
 				<?php if ( ! empty( $image['url'] ) ) : ?>
-					<div class="crisis-services-image-text__image">
+					<div class="overlapping-text-image-tiles__image">
 						<img src="<?php echo esc_url( $image['url'] ); ?>" class="block mx-auto" alt="<?php echo ( ! empty( $image['alt'] ) ) ? esc_attr( $image['alt'] ) : esc_attr( $title ); ?>">
 					</div>
 				<?php endif; ?>
