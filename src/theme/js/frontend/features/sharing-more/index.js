b91512a5c796afb94874e6f30a573edc9650cc54
ct-jacebuquia <74394071+ct-jacebuquia@users.noreply.github.com>
2021-08-24 17:04:02 +0800

    KTTL-1225 - [FE] Resource Center | Article Header > Share styling and functionality issues (#804)
    Co-authored-by: Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>

--- src/theme/js/frontend/features/sharing-more/index.js
+++ src/theme/js/frontend/features/sharing-more/index.js
@@ -1,52 +1,76 @@
 import $ from 'jquery';
-import tippy, {roundArrow} from 'tippy.js';
-import {copyToClipboard} from 'plugin/js/main/utils';
+import tippy, { roundArrow } from 'tippy.js';
+import { copyToClipboard } from 'plugin/js/main/utils';
 
 const hasNavigatorShare = !!navigator.share;
 
 export default function sharingMore(button, content, tippyOptions = {}) {
 	// Use native sharing if available
 	if (hasNavigatorShare) {
-		$(button).on('click', () => navigator.share({
-			url: $('head link[rel="canonical"]').attr('href') || window.location.href,
-		}));
+		$(button).on('click', () =>
+			navigator.share({
+				url:
+					$('head link[rel="canonical"]').attr('href') ||
+					window.location.href,
+			})
+		);
 	} else {
 		// Init tippy
-		tippy(button, Object.assign({
-			content,
-			interactive: true,
-			placement: 'right',
-			appendTo: 'parent',
-			arrow: roundArrow,
-		}, tippyOptions));
+		tippy(
+			button,
+			Object.assign(
+				{
+					content,
+					interactive: true,
+					placement: 'right',
+					appendTo: 'parent',
+					arrow: roundArrow,
+				},
+				tippyOptions
+			)
+		);
 
 		// Event Handlers
-		$(content).find('tr[data-row]').on('click', (e) => {
-			e.preventDefault;
-			const type = $(e.currentTarget).attr('data-row');
-			const canonicalURL = $(content).attr('data-url') || document.querySelector('link[rel="canonical"]').href;
+		$(content)
+			.find('tr[data-row]')
+			.on('click', (e) => {
+				e.preventDefault;
+				const type = $(e.currentTarget).attr('data-row');
+				const canonicalURL =
+					$(content).attr('data-url') ||
+					document.querySelector('link[rel="canonical"]')?.href ||
+					document
+						.querySelector('[property="og:url"]')
+						?.getAttribute('content');
 
-			switch (type) {
-				case 'facebook':
-				case 'twitter':
-					$(`.post-social-share-btn[data-type="${type}"]`).click();
-					break;
-				case 'clipboard':
-					copyToClipboard(canonicalURL);
-					const $elem = $(content).find('[data-row="clipboard"] > td:nth-child(2)');
-					const copiedCB = $elem.data('copiedCB');
-					copiedCB && clearTimeout(copiedCB);
-					$elem.css({'color': 'rgba(16, 16, 102, 0.8)'});
-					$elem.data('copiedCB', setTimeout(() => $elem.text('Copy Link'), 2000));
-					setTimeout(() => {
-						$elem.css({'color': 'inherit'});
-					}, 2000);
-					$elem.text('Link Copied!');
-					break;
-				case 'email':
-					window.location = `mailto:?body=${canonicalURL}`;
-					break;
-			}
-		});
+				switch (type) {
+					case 'facebook':
+					case 'twitter':
+						$(
+							`.post-social-share-btn[data-type="${type}"]`
+						).click();
+						break;
+					case 'clipboard':
+						copyToClipboard(canonicalURL);
+						const $elem = $(content).find(
+							'[data-row="clipboard"] > td:nth-child(2)'
+						);
+						const copiedCB = $elem.data('copiedCB');
+						copiedCB && clearTimeout(copiedCB);
+						$elem.css({ color: 'rgba(16, 16, 102, 0.8)' });
+						$elem.data(
+							'copiedCB',
+							setTimeout(() => $elem.text('Copy Link'), 2000)
+						);
+						setTimeout(() => {
+							$elem.css({ color: 'inherit' });
+						}, 2000);
+						$elem.text('Link Copied!');
+						break;
+					case 'email':
+						window.location = `mailto:?body=${canonicalURL}`;
+						break;
+				}
+			});
 	}
 }
