a33c1354193a92662809883b4b31c7fcd959f0c4
Joshua Cerbito <joshuacerbito@gmail.com>
2021-09-15 08:42:45 +0800

    Add HSaito's Blank Page

---
+++ theme/blank.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Empty Template (no branding or foundation)
+*/
+?>
+
+<head>
+
+	<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+</head>
+
+<body id="tech">
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile;?>
+<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+
+</body>
