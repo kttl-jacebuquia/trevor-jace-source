3bc10b3e6c59b1c32c3b1ab58899189f3cce372f
Joshua Cerbito <joshuacerbito@gmail.com>
2021-09-17 16:48:22 +0800

    Update HSaito's Pages

--- theme/blank.php
+++ theme/blank.php
@@ -9,13 +9,15 @@ Template Name: Empty Template (no branding or foundation)
 	<?php wp_head(); ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	
+
 </head>
 
 <body id="tech">
-<?php while ( have_posts() ) : the_post(); ?>
+<?php
+while ( have_posts() ) :
+	the_post();
+	?>
 	<?php the_content(); ?>
-<?php endwhile;?>
+<?php endwhile; ?>
 <?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-
 </body>
