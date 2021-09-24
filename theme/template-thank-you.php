a8e850d6f8db2b83a638565558df41e0cf1cb5d6
Ralph Allen Valencia <ralph.allen.valencia@codeandtheory.com>
2021-09-24 11:21:25 +0800

    KTTL-1752 Add Thank You Page Template

---
+++ theme/template-thank-you.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Thank You Page Template
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="text-white">
+    <style type="text/css" media="screen">
+    p { padding-bottom: 1rem;
+        font-size: inherit;
+        line-height: 1.6;
+        text-rendering: optimizeLegibility;
+    }
+    </style>
+    <p class="has-text-align-center"></p><p class="has-text-align-center">You have reached the end of the survey! Thank you for your time.</p>
+    <p class="has-text-align-center">If you have questions, please contact Amy Green, PhD at <a target="_blank" href="tel:3102718845" rel="noreferrer noopener">310-271-8845</a> x242. You can also contact the ethics committee, Solutions IRB, at <a target="_blank" href="tel:8552264472" rel="noreferrer noopener">(855) 226-4472</a>.</p><p></p>
+</div>
+
+<script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","licenseKey":"NRJS-2dda74bbb5d29f36578","applicationID":"1002335747","transactionName":"M1EEMkVXV0dXAEJbXgobJwVDX1ZaGQFaU18P","queueTime":0,"applicationTime":156,"atts":"HxYHRA1NREk=","errorBeacon":"bam.nr-data.net","agent":""}</script>
+
+<?php get_footer(); ?>
