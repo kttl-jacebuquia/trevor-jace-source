79dd78b3421e341ba2d74238597ee396a82b000d
Kim Morales <kim.llante.morales@gmail.com>
2021-10-07 14:36:58 +0800

    KTTL-1683: Added bouncing-arrow class for the click behavior. (#1106)

--- theme/inc/Helper/Page_Header.php
+++ theme/inc/Helper/Page_Header.php
@@ -586,7 +586,7 @@ class Page_Header {
 						<?php endforeach; ?>
 					</div>
 					<aside class="page-header__bottom">
-						<button class="page-header__scroll-down trevor-ti-chevron-down animate-bounce"></button>
+						<button class="page-header__scroll-down trevor-ti-chevron-down animate-bounce bouncing-arrow"></button>
 					</aside>
 				</div>
 			</div>
