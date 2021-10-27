1ab67440073978e21fb68535ed08df02a8012ee6
Jace Buquia <90045797+kttl-jacebuquia@users.noreply.github.com>
2021-10-27 19:53:38 +0800

    KTTL-1931 - Promo Pop Up / Clicking the CTA does not dismiss the pop up (#1201)
    Co-authored-by: Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>

--- src/theme/js/frontend/features/promo-popup.js
+++ src/theme/js/frontend/features/promo-popup.js
@@ -6,37 +6,59 @@ const POPUP_MODAL_DISMISSED_KEY = 'promo-popup-modal-dismissed';
 
 class PromoPopup {
 	static selector = '.promo-popup-modal';
+	static innerLinksSelector = 'a[href]';
 
 	static init() {
 		this.canShow = !localStorage.getItem(POPUP_MODAL_DISMISSED_KEY);
 
 		this.$content = $(this.selector);
+		this.$innerLinks = this.$content.find(this.innerLinksSelector);
 
-		if ( this.$content.length ) {
+		if (this.$content.length) {
 			modal(this.$content, {
 				onInit: this.onInit.bind(this),
 				onClose: this.onClose.bind(this),
 			});
+
+			this.handleInnerLinks();
 		}
 	}
 
+	static handleInnerLinks() {
+		this.$innerLinks.on('click', this.onClose.bind(this));
+	}
+
 	static showWhenAvailable(withDelay = false) {
-		if ( this.modal && this.canShow ) {
+		if (this.modal && this.canShow) {
 			if (withDelay) {
-				setTimeout(() => this.modal.open(), MODAL_DELAY)
-			}
-			else {
+				setTimeout(() => this.modal.open(), MODAL_DELAY);
+			} else {
 				this.modal.open();
 			}
 		}
 	}
 
-	static onInit(modal) {
-		this.modal = modal;
+	static onInit(_modal) {
+		this.modal = _modal;
 	}
 
-	static onClose() {
+	static onClose(e) {
+		e.preventDefault();
+		const { target, href } = e.currentTarget;
+
+		if ( !href ) {
+			return false;
+		}
+
 		localStorage.setItem(POPUP_MODAL_DISMISSED_KEY, true);
+
+		// Determine if link loads in the same window or not
+		if ( target === '_blank' ) {
+			window.open(e.currentTarget);
+		}
+		else {
+			window.location.href = href;
+		}
 	}
 }
 
