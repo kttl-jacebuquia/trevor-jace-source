af3cf121f70b9527b35caa813500f0cd85e459a9
Jace Buquia <90045797+kttl-jacebuquia@users.noreply.github.com>
2021-12-13 19:40:57 +0800

    KTTL-1831 - Article Component / Create video component for articles (#1316)
    Co-authored-by: Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>

--- theme/inc/ACF/Field_Group/Embed.php
+++ theme/inc/ACF/Field_Group/Embed.php
@@ -2,7 +2,7 @@
 
 namespace TrevorWP\Theme\ACF\Field_Group;
 
-use TrevorWP\Theme\ACF\Util\Field_Val_Getter;
+use TrevorWP\CPT\RC\Article;
 
 class Embed extends A_Basic_Section implements I_Block, I_Renderable {
 	const FIELD_EMBED_URL    = 'embed_url';
@@ -62,7 +62,7 @@ class Embed extends A_Basic_Section implements I_Block, I_Renderable {
 			'title'      => 'Custom Embed',
 			'category'   => 'common',
 			'icon'       => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="48" height="48" role="img" aria-hidden="true" focusable="false"><path d="M20.8 10.7l-4.3-4.3-1.1 1.1 4.3 4.3c.1.1.1.3 0 .4l-4.3 4.3 1.1 1.1 4.3-4.3c.7-.8.7-1.9 0-2.6zM4.2 11.8l4.3-4.3-1-1-4.3 4.3c-.7.7-.7 1.8 0 2.5l4.3 4.3 1.1-1.1-4.3-4.3c-.2-.1-.2-.3-.1-.4z"></path></svg>',
-			'post_types' => array( 'page' ),
+			'post_types' => array( 'page', Article::POST_TYPE ),
 		);
 	}
 
@@ -80,7 +80,7 @@ class Embed extends A_Basic_Section implements I_Block, I_Renderable {
 
 		# Build embed classnames
 		$embed_cls   = array( 'embed-content w-full' );
-		$embed_cls[] = 'rounded-px10 overflow-hidden';
+		$embed_cls[] = 'overflow-hidden';
 		$embed_cls[] = 'xl:tracking-em_001';
 		$embed_cls   = implode( ' ', $embed_cls );
 
