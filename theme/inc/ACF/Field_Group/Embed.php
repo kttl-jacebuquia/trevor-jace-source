b9997b3118e9019008d38c43f15555ca62474f85
Ralph Allen Valencia <ralph.allen.valencia@codeandtheory.com>
2021-12-20 16:01:52 +0800

    KTTL-2183: Add Custom Embed in Post (#1335)

--- theme/inc/ACF/Field_Group/Embed.php
+++ theme/inc/ACF/Field_Group/Embed.php
@@ -2,6 +2,7 @@
 
 namespace TrevorWP\Theme\ACF\Field_Group;
 
+use TrevorWP\CPT\Post;
 use TrevorWP\CPT\RC\Article;
 
 class Embed extends A_Basic_Section implements I_Block, I_Renderable {
@@ -64,7 +65,7 @@ class Embed extends A_Basic_Section implements I_Block, I_Renderable {
 			'title'      => 'Custom Embed',
 			'category'   => 'common',
 			'icon'       => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="48" height="48" role="img" aria-hidden="true" focusable="false"><path d="M20.8 10.7l-4.3-4.3-1.1 1.1 4.3 4.3c.1.1.1.3 0 .4l-4.3 4.3 1.1 1.1 4.3-4.3c.7-.8.7-1.9 0-2.6zM4.2 11.8l4.3-4.3-1-1-4.3 4.3c-.7.7-.7 1.8 0 2.5l4.3 4.3 1.1-1.1-4.3-4.3c-.2-.1-.2-.3-.1-.4z"></path></svg>',
-			'post_types' => array( 'page', Article::POST_TYPE ),
+			'post_types' => array( 'page', Article::POST_TYPE, Post::POST_TYPE ),
 		);
 	}
 
