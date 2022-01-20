a576eabb6771db78c3946959b70e5a0db0e3276b
Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>
2022-01-20 15:08:52 +0800

    KTTL-2220 - [FE - Desktop and Tablet] Topic Cards > Bills and Letters > Share - The copy link and email functionalities are not working

--- src/plugin/js/main/utils/copy-to-clipboard.js
+++ src/plugin/js/main/utils/copy-to-clipboard.js
@@ -1,13 +1,21 @@
 // https://stackoverflow.com/a/46118025
 export default function copyToClipboard(text) {
-	var dummy = document.createElement("textarea");
+	const dummy = document.createElement('input');
 	// to avoid breaking orgain page when copying more words
 	// cant copy when adding below this code
 	// dummy.style.display = 'none'
 	document.body.appendChild(dummy);
 	//Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-	dummy.value = text;
-	dummy.select();
-	document.execCommand("copy");
+	dummy.value = text.trim();
+
+	// source: https://stackoverflow.com/a/47880284
+	const selection = document.getSelection();
+	const range = document.createRange();
+	range.selectNode(dummy);
+	selection.removeAllRanges();
+	selection.addRange(range);
+
+	document.execCommand('copy');
+	selection.removeAllRanges();
 	document.body.removeChild(dummy);
 }
