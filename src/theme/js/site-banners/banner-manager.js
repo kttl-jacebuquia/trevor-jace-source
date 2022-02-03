ef028a2762b83785b863cd9794f1d48cedbae9b7
Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>
2022-02-03 17:22:47 +0800

    KTTL-1993 - [a11y - VO, Mobile] Global > Site Banners - VO does not focus on the contents of the page after the close button is clicked

--- src/theme/js/site-banners/banner-manager.js
+++ src/theme/js/site-banners/banner-manager.js
@@ -7,6 +7,7 @@ export default class SiteBanner {
 		this.bannerObj = bannerObj;
 		this.sessionName = `sessionBannerObj-${this.bannerObj.id}`;
 		this.containerHeight = 0;
+		this.id = bannerObj.id;
 
 		if (!this.isClosed()) {
 			this.build();
@@ -15,7 +16,6 @@ export default class SiteBanner {
 	}
 
 	insert() {
-
 		if (this.$banner.data('type') === 'custom') {
 			this.$banner.prependTo(this.$container);
 		} else {
@@ -26,24 +26,29 @@ export default class SiteBanner {
 	}
 
 	build() {
-		const {bannerObj} = this;
+		const { bannerObj } = this;
 
-		this.$banner = $("<div/>", {
+		this.$banner = $('<div/>', {
 			class: `site-banner__${bannerObj.type}`,
 			id: `site-banner-${bannerObj.id}`,
 			'data-type': bannerObj.type,
 			'data-id': bannerObj.id,
+			tabindex: 0,
 		});
-		const $container = $("<div class='site-banner__container'>" +
-			"<i class='site-banner__warning'></i>" +
-			"</div>").appendTo(this.$banner);
+		const $container = $(
+			"<div class='site-banner__container'>" +
+				"<i class='site-banner__warning'></i>" +
+				'</div>'
+		).appendTo(this.$banner);
 
 		// Text
-		const $text = $("<p class='site-banner__text'></p>").appendTo($container);
+		const $text = $("<p class='site-banner__text'></p>").appendTo(
+			$container
+		);
 
 		// Title
-		if ( bannerObj.title ) {
-			$("<span/>", {
+		if (bannerObj.title) {
+			$('<span/>', {
 				class: 'site-banner__title',
 				text: `${bannerObj.title} `,
 			}).appendTo($text);
@@ -51,15 +56,17 @@ export default class SiteBanner {
 
 		// Description
 		if (bannerObj.desc) {
-			$("<span/>", {
+			$('<span/>', {
 				class: 'site-banner__description',
 				html: bannerObj.desc,
 			}).appendTo($text);
 		}
 
-		$("<button aria-label='click to close banner' class='site-banner__close-btn'>" +
-			"<i class='trevor-ti-x text-indigo'></i>" +
-			"</button>")
+		$(
+			"<button aria-label='click to close banner' class='site-banner__close-btn'>" +
+				"<i class='trevor-ti-x text-indigo'></i>" +
+				'</button>'
+		)
 			.on('click', this.handleCloseClick)
 			.appendTo($container);
 	}
@@ -70,7 +77,7 @@ export default class SiteBanner {
 		sessionStorage.setItem(this.sessionName, Date.now());
 		this.remove();
 		this.updateSiteBannerCSSvariable();
-	}
+	};
 
 	isClosed() {
 		return Object.keys(sessionStorage).includes(this.sessionName);
@@ -78,11 +85,21 @@ export default class SiteBanner {
 
 	remove() {
 		this.$banner.remove();
+		this.$banner.trigger('remove');
 	}
 
 	updateSiteBannerCSSvariable() {
 		const root = document.documentElement;
 		this.containerHeight = this.$container.height();
-		root.style.setProperty('--site-banner-height', `${this.containerHeight}px`);
+		root.style.setProperty(
+			'--site-banner-height',
+			`${this.containerHeight}px`
+		);
+	}
+
+	on(event, callback) {
+		this.$banner.on(event, () => {
+			callback.apply(this, [this]);
+		});
 	}
 }
