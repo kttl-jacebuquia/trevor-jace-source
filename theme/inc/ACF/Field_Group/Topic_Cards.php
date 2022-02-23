04cf35d20efa768cde34cba1ded6f8d786ba1dfa
Jayson Buquia <jaysonbuquia@JaysonBiasMBP13.bbrouter>
2022-02-23 12:39:37 +0800

    KTTL-2292,KTTL-2302 - Carousel arrow issues

--- theme/inc/ACF/Field_Group/Topic_Cards.php
+++ theme/inc/ACF/Field_Group/Topic_Cards.php
@@ -652,7 +652,7 @@ class Topic_Cards extends A_Field_Group implements I_Block, I_Renderable {
 						<?php endforeach; ?>
 					</div>
 					<?php if ( 'carousel' === $layout ) : ?>
-						<?php static::render_carousel_pagination(); ?>
+						<?php echo static::render_carousel_navigation(); ?>
 					<?php endif; ?>
 				</div>
 				<?php if ( 'carousel' === $layout ) : ?>
