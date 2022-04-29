d79b6f273fb0fa776a5b507c425786be60738a63
Ted Whitehead <tedwhitehead@gmail.com>
2022-04-29 17:19:19 -0400

    create new Circle of Light template and apply minor style updates

---
+++ theme/template-circle-of-light.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Circle of Light
+*/
+?>
+
+<?php
+	$hero = \TrevorWP\Theme\ACF\Field_Group\Page_Header::render();
+	get_header();
+?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+	<main class="bg-white" role="main" id="site-content" tabindex="0">
+		<?php echo $hero; ?>
+		<?php the_content(); ?>
+	</main>
+<?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
